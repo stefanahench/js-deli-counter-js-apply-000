39716c7fac52f0b7cd176e09bd9e2c5a3b989b81
Stefan Hench <stefan.hench@gmail.com>
2019-02-05 16:46:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function takeANumber (line, name) {
-  line.push(name)
-  return `Welcome, ${name}. You are number ${line.length} in line.`
-}
+// function takeANumber (line, name) {
+//   line.push(name)
+//   return `Welcome, ${name}. You are number ${line.length} in line.`
+// }
 
 function nowServing (line) {
   if (line.length>0) {
